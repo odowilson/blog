390f641b0e090825839abb4829721bafa9a6329f
WilsonGovment <58129855+WilsonGovment@users.noreply.github.com>
2020-05-29 20:57:25 +0100

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -71,9 +71,11 @@ app.post("/compose", function(request, response) {
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
